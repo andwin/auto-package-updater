ef11a779acf8bb25d1e286f63466fd83e9160539
Anders Windelhed <anders.windelhed@gmail.com>
2025-12-29 23:48:38 +0100

    Verifying pristine state

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@
 
 import detectPackageManager from './utils/detect_package_panager'
 import verifyGitRepo from './utils/verify_git_repo'
+import verifyPristineState from './utils/verify_pristine_state'
 
 const run = async () => {
   await verifyGitRepo()
+  await verifyPristineState()
   const packageManager = await detectPackageManager()
   console.log(`Using ${packageManager} as package manager`)
 }
