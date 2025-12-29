f7e5257604c36f3015fa4b6489ec58f4cf13fb3c
Anders Windelhed <anders.windelhed@gmail.com>
2025-12-29 22:44:03 +0100

    Detecting package manager

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
+import detectPackageManager from './utils/detect_package_panager'
 import verifyGitRepo from './utils/verify_git_repo'
 
 const run = async () => {
   await verifyGitRepo()
+  const packageManager = await detectPackageManager()
+  console.log(`Using ${packageManager} as package manager`)
 }
 
 run()
---
+++ src/utils/detect_package_panager.ts
@@ -0,0 +1,25 @@
+import { access } from 'node:fs/promises'
+import { join } from 'node:path'
+
+type PackageManager = 'pnpm' | 'yarn' | 'npm'
+
+const detectPackageManager = async (): Promise<PackageManager> => {
+  if (await exists('pnpm-lock.yaml')) return 'pnpm'
+  if (await exists('yarn.lock')) return 'yarn'
+  if (await exists('package-lock.json')) return 'npm'
+
+  console.error('No package manager found')
+  console.error('Please run this command in the root of your project with a package manager lock file')
+  process.exit(1)
+}
+
+const exists = async (filename: string) => {
+  try {
+    await access(join(process.cwd(), filename))
+    return true
+  } catch {
+    return false
+  }
+}
+
+export default detectPackageManager;
