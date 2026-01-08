58a777199a32c91aa63f52fd3ebe753d0f4184e5
Anders Windelhed <anders.windelhed@gmail.com>
2026-01-08 20:27:22 +0100

    Removing debug output

--- src/utils/run_custom_command.ts
+++ src/utils/run_custom_command.ts
@@ -4,9 +4,6 @@ const runCustomCommand = async (command: string) => {
   try {
     await execa(command, { shell: true })
   } catch (e) {
-    const error = e as Error & { stdout?: string }
-    console.log('stdout', error)
-
     ;(e as Error).message = `Custom command failed: ${command}`
     throw e
   }
